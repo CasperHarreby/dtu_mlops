45ee049f881c57ce72300e55659c4f24637d803d
Casper Harreby <casperharreby99@gmail.com>
2025-01-05 14:40:34 +0100

    completed the typing exercise and ruff formatting.

--- s2_organisation_and_version_control/exercise_files/typing_exercise.py
+++ s2_organisation_and_version_control/exercise_files/typing_exercise.py
@@ -1,5 +1,8 @@
+from typing import Callable, Optional, Tuple, Union, List  # you will need all of them in your code
+
 import torch
 from torch import nn
+import torch.utils
 
 
 class Network(nn.Module):
@@ -12,7 +15,12 @@ class Network(nn.Module):
 
     """
 
-    def __init__(self, input_size, output_size, hidden_layers, drop_p=0.5) -> None:
+    def __init__(
+        self, input_size: int,
+        output_size: int,
+        hidden_layers: int,
+        drop_p: float = 0.5
+    ) -> None:
         super().__init__()
         # Input to a hidden layer
         self.hidden_layers = nn.ModuleList([nn.Linear(input_size, hidden_layers[0])])
@@ -25,7 +33,7 @@ class Network(nn.Module):
 
         self.dropout = nn.Dropout(p=drop_p)
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor):
         """Forward pass through the network, returns the output logits."""
         for each in self.hidden_layers:
             x = nn.functional.relu(each(x))
@@ -35,7 +43,11 @@ class Network(nn.Module):
         return nn.functional.log_softmax(x, dim=1)
 
 
-def validation(model, testloader, criterion):
+def validation(
+    model: nn.Module,
+    testloader: torch.utils.data.dataloader,
+    criterion
+) -> tuple[float, float]:
     """Validation pass through the dataset."""
     accuracy = 0
     test_loss = 0
@@ -56,7 +68,15 @@ def validation(model, testloader, criterion):
     return test_loss, accuracy
 
 
-def train(model, trainloader, testloader, criterion, optimizer=None, epochs=5, print_every=40) -> None:
+def train(
+    model: nn.Module,
+    trainloader: torch.utils.data.dataloader,
+    testloader: torch.utils.data.dataloader,
+    criterion: Callable | nn.Module,
+    optimizer: None | torch.optim.Optimizer = None,
+    epochs: int = 5,
+    print_every: int = 40
+) -> None:
     """Train a PyTorch Model."""
     if optimizer is None:
         optimizer = torch.optim.Adam(model.parameters(), lr=1e-2)
