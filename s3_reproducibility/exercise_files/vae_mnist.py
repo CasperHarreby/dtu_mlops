fe0d1c403f82f974b3287e797c72927d63370654
Casper Harreby <casperharreby99@gmail.com>
2025-01-11 10:51:52 +0100

    updated default_config.yaml

--- s3_reproducibility/exercise_files/vae_mnist.py
+++ s3_reproducibility/exercise_files/vae_mnist.py
@@ -14,81 +14,100 @@ from torch.utils.data import DataLoader
 from torchvision.datasets import MNIST
 from torchvision.utils import save_image
 
-# Model Hyperparameters
-dataset_path = "~/datasets"
-cuda = True
-DEVICE = torch.device("cuda" if cuda else "cpu")
-batch_size = 100
-x_dim = 784
-hidden_dim = 400
-
-# Data loading
-mnist_transform = transforms.Compose([transforms.ToTensor()])
-
-train_dataset = MNIST(dataset_path, transform=mnist_transform, train=True, download=True)
-test_dataset = MNIST(dataset_path, transform=mnist_transform, train=False, download=True)
-
-train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
-test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
-
-encoder = Encoder(input_dim=x_dim, hidden_dim=hidden_dim, latent_dim=20)
-decoder = Decoder(latent_dim=20, hidden_dim=hidden_dim, output_dim=x_dim)
-
-model = Model(Encoder=encoder, Decoder=decoder).to(DEVICE)
+import hydra
+from omegaconf import OmegaConf
+import logging
+log = logging.getLogger(__name__)
 
 
-def loss_function(x, x_hat, mean, log_var):
-    """Elbo loss function."""
-    reproduction_loss = nn.functional.binary_cross_entropy(x_hat, x, reduction="sum")
-    kld = -0.5 * torch.sum(1 + log_var - mean.pow(2) - log_var.exp())
-    return reproduction_loss + kld
+@hydra.main(config_path="config", config_name="default_config.yaml")
+def train(config) -> None:
+    print(f"configuration: \n {OmegaConf.to_yaml(config)}")
+    
+    # Model Hyperparameters
+    hparams = config
+    torch.manual_seed(hparams["seed"])
+    DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+    
+    # Data loading
+    mnist_transform = transforms.Compose([transforms.ToTensor()])
+
+    train_dataset = MNIST(hparams["dataset_path"], transform=mnist_transform, train=True, download=True)
+    test_dataset = MNIST(hparams["dataset_path"], transform=mnist_transform, train=False, download=True)
+
+    train_loader = DataLoader(dataset=train_dataset, batch_size=hparams["batch_size"], shuffle=True)
+    test_loader = DataLoader(dataset=test_dataset, batch_size=hparams["batch_size"], shuffle=False)
+
+    encoder = Encoder(
+        input_dim=hparams["x_dim"],
+        hidden_dim=hparams["hidden_dim"],
+        latent_dim=hparams["latent_dim"]
+    )
+    
+    decoder = Decoder(
+        latent_dim=hparams["latent_dim"],
+        hidden_dim=hparams["hidden_dim"],
+        output_dim=hparams["x_dim"]
+    )
+
+    model = Model(encoder=encoder, decoder=decoder).to(DEVICE)
+
+    def loss_function(x, x_hat, mean, log_var):
+        """Elbo loss function."""
+        reproduction_loss = nn.functional.binary_cross_entropy(x_hat, x, reduction="sum")
+        kld = -0.5 * torch.sum(1 + log_var - mean.pow(2) - log_var.exp())
+        return reproduction_loss + kld
 
 
-optimizer = Adam(model.parameters(), lr=1e-3)
+    optimizer = Adam(model.parameters(), lr=1e-3)
 
 
-print("Start training VAE...")
-model.train()
-for epoch in range(20):
-    overall_loss = 0
-    for batch_idx, (x, _) in enumerate(train_loader):
-        if batch_idx % 100 == 0:
-            print(batch_idx)
-        x = x.view(batch_size, x_dim)
-        x = x.to(DEVICE)
+    print("Start training VAE...")
+    model.train()
+    for epoch in range(hparams['epochs']):
+        overall_loss = 0
+        for batch_idx, (x, _) in enumerate(train_loader):
+            if batch_idx % 100 == 0:
+                print(batch_idx)
+            x = x.view(hparams['batch_size'], hparams['x_dim'])
+            x = x.to(DEVICE)
 
-        optimizer.zero_grad()
+            optimizer.zero_grad()
 
-        x_hat, mean, log_var = model(x)
-        loss = loss_function(x, x_hat, mean, log_var)
+            x_hat, mean, log_var = model(x)
+            loss = loss_function(x, x_hat, mean, log_var)
 
-        overall_loss += loss.item()
+            overall_loss += loss.item()
 
-        loss.backward()
-        optimizer.step()
-    print(f"Epoch {epoch+1} complete!,  Average Loss: {overall_loss / (batch_idx*batch_size)}")
-print("Finish!!")
+            loss.backward()
+            optimizer.step()
+        print(f"Epoch {epoch+1} complete!,  Average Loss: {overall_loss / (batch_idx*hparams['batch_size'])}")
+    print("Finish!!")
 
-# save weights
-torch.save(model, f"{os.getcwd()}/trained_model.pt")
+    # save weights
+    torch.save(model, f"{os.getcwd()}/trained_model.pt")
 
-# Generate reconstructions
-model.eval()
-with torch.no_grad():
-    for batch_idx, (x, _) in enumerate(test_loader):
-        if batch_idx % 100 == 0:
-            print(batch_idx)
-        x = x.view(batch_size, x_dim)
-        x = x.to(DEVICE)
-        x_hat, _, _ = model(x)
-        break
+    # Generate reconstructions
+    model.eval()
+    with torch.no_grad():
+        for batch_idx, (x, _) in enumerate(test_loader):
+            if batch_idx % 100 == 0:
+                print(batch_idx)
+            x = x.view(hparams['batch_size'], hparams['x_dim'])
+            x = x.to(DEVICE)
+            x_hat, _, _ = model(x)
+            break
 
-save_image(x.view(batch_size, 1, 28, 28), "orig_data.png")
-save_image(x_hat.view(batch_size, 1, 28, 28), "reconstructions.png")
+    save_image(x.view(hparams['batch_size'], 1, 28, 28), "orig_data.png")
+    save_image(x_hat.view(hparams['batch_size'], 1, 28, 28), "reconstructions.png")
 
-# Generate samples
-with torch.no_grad():
-    noise = torch.randn(batch_size, 20).to(DEVICE)
-    generated_images = decoder(noise)
+    # Generate samples
+    with torch.no_grad():
+        noise = torch.randn(hparams['batch_size'], 20).to(DEVICE)
+        generated_images = decoder(noise)
 
-save_image(generated_images.view(batch_size, 1, 28, 28), "generated_sample.png")
+    save_image(generated_images.view(hparams['batch_size'], 1, 28, 28), "generated_sample.png")
+
+
+if __name__ == "__main__":
+    train()
